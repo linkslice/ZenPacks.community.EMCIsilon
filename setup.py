a29bf406cd8ef6147019bbd072397c456045106e
linkslice <sparctacus@gmail.com>
2016-10-12 16:35:29 -0700

    upped version and license, also updated egg

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@
 # or saved.  Do not modify them directly here.
 # NB: PACKAGES is deprecated
 NAME = "ZenPacks.community.EMCIsilon"
-VERSION = "1.0.1"
+VERSION = "1.2"
 AUTHOR = "Bryan Irvine"
-LICENSE = ""
+LICENSE = "MIT"
 NAMESPACE_PACKAGES = ['ZenPacks', 'ZenPacks.community']
 PACKAGES = ['ZenPacks', 'ZenPacks.community', 'ZenPacks.community.EMCIsilon']
-INSTALL_REQUIRES = ""
+INSTALL_REQUIRES = []
 COMPAT_ZENOSS_VERS = ""
 PREV_ZENPACK_NAME = ""
 # STOP_REPLACEMENTS
